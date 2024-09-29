3d25077a482997e7b059f8b063a7915cf9b69fc9
TonyLe <149919663+TrungKien0206@users.noreply.github.com>
2024-09-29 14:21:07 +0700

    Add files via upload

--- src/btver5/Person.java
+++ src/btver5/Person.java
@@ -11,6 +11,64 @@ abstract class Person implements IPerson {
     protected Date bookBorrowDate;
     protected Date bookReturnDate;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Date getBookBorrowDate() {
+        return bookBorrowDate;
+    }
+
+    public void setBookBorrowDate(Date bookBorrowDate) {
+        this.bookBorrowDate = bookBorrowDate;
+    }
+
+    public Date getBookReturnDate() {
+        return bookReturnDate;
+    }
+
+    public void setBookReturnDate(Date bookReturnDate) {
+        this.bookReturnDate = bookReturnDate;
+    }
+
+    public SimpleDateFormat getDateFormat() {
+        return dateFormat;
+    }
+
+    public void setDateFormat(SimpleDateFormat dateFormat) {
+        this.dateFormat = dateFormat;
+    }
+
+    @Override
+    public abstract void displayInfo();
+
+    @Override
+    public abstract void updatePerson(String id);
+
+    @Override
+    public abstract void addPerson();
+
+    
     SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
 
     public boolean isBookOverdue() {
